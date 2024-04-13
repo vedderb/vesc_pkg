685510ce107ed713f17f0d4245dcd8fe0e3cced6
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-13 13:56:41 -0700

    Update utils.c

--- tnt/tnt/utils.c
+++ tnt/tnt/utils.c
@@ -23,6 +23,11 @@ uint32_t rnd(uint32_t seed) {
     return seed * 1664525u + 1013904223u;
 }
 
+float lerp(float x1, float x2, float y1, float y2, float val){
+    float t= (val - x1)/(x2 - x1);
+    return (y1 * (1 - t) + y2 * t);
+}
+
 void rate_limitf(float *value, float target, float step) {
     if (fabsf(target - *value) < step) {
         *value = target;
