0e2ef577e32f495f1ff2c978cf75ada3a5e38e0a
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-13 14:08:40 -0700

    Update proportional_gain.c

--- tnt/tnt/proportional_gain.c
+++ tnt/tnt/proportional_gain.c
@@ -15,6 +15,9 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <http://www.gnu.org/licenses/>.
 
+#include "proportional_gain.h"
+#include "utils.h"
+
 static float pitch_kp_select(float abs_prop_smooth, KpArray *k) {
 	float kp_mod = 0;
 	float kp_min = 0;
