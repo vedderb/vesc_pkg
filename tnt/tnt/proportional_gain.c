f8f4d9d36ff19ea8976c8974b25bfe624b702e73
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-13 14:59:25 -0700

    Update proportional_gain.c

--- tnt/tnt/proportional_gain.c
+++ tnt/tnt/proportional_gain.c
@@ -24,18 +24,18 @@ float pitch_kp_select(float abs_prop_smooth, KpArray k) {
 	float scale_angle_min = 0;
 	float scale_angle_max = 1;
 	float kp_max = 0;
-	int i = k->count;
+	int i = k.count;
 	//Determine the correct current to use based on prop_smooth
 	while (i >= 0) {
-		if (abs_prop_smooth>= k->pitch_kp[i][0]) {
-			kp_min = k->pitch_kp[i][1];
-			scale_angle_min = k->pitch_kp[i][0];
-			if (i == k->count) { //if we are at the highest current only use highest kp
-				kp_max = k->pitch_kp[i][1];
+		if (abs_prop_smooth>= k.pitch_kp[i][0]) {
+			kp_min = k.pitch_kp[i][1];
+			scale_angle_min = k.pitch_kp[i][0];
+			if (i == k.count) { //if we are at the highest current only use highest kp
+				kp_max = k.pitch_kp[i][1];
 				scale_angle_max = 90;
 			} else {
-				kp_max = k->pitch_kp[i+1][1];
-				scale_angle_max = k->pitch_kp[i+1][0];
+				kp_max = k.pitch_kp[i+1][1];
+				scale_angle_max = k.pitch_kp[i+1][0];
 			}
 			i=-1;
 		}
@@ -72,26 +72,26 @@ KpArray pitch_kp_configure(const tnt_config *config, int mode){
 		};
 	}
 	//Check for current inputs
-	k->count=0;
+	k.count=0;
 	int i = 1;
 	while (i <= 6){
 		if (pitch_current[i][1]!=0 && pitch_current[i][0]>pitch_current[i-1][0]) {
-			k->count = i;
-			k->pitch_kp[i][0]=pitch_current[i][0];
-			if (config->pitch_kp_input) {
-				k->pitch_kp[i][1]=pitch_current[i][1];
-			} else {k->pitch_kp[i][1]=pitch_current[i][1]/pitch_current[i][0];}
+			k.count = i;
+			k.pitch_kp[i][0]=pitch_current[i][0];
+			if (config.pitch_kp_input) {
+				k.pitch_kp[i][1]=pitch_current[i][1];
+			} else {k.pitch_kp[i][1]=pitch_current[i][1]/pitch_current[i][0];}
 		} else { i=7; }
 		i++;
 	}
 	//Check kp0 for an appropriate value, prioritizing kp1
-	if (k->count > 0) {
-		if (k->pitch_kp[1][1]<config->kp0) {
-			k->pitch_kp[0][1]= k->pitch_kp[1][1];
-		} else { k->pitch_kp[0][1] = config->kp0; }
-	} else if (k->count == 0 && k->pitch_kp[0][1]==0) { //If no currents 
-		k->pitch_kp[0][1] = 5; //If no kp use 5
-	} else { k->pitch_kp[0][1] = config->kp0; }
+	if (k.count > 0) {
+		if (k.pitch_kp[1][1]<config->kp0) {
+			k.pitch_kp[0][1]= k.pitch_kp[1][1];
+		} else { k.pitch_kp[0][1] = config->kp0; }
+	} else if (k.count == 0 && k.pitch_kp[0][1]==0) { //If no currents 
+		k.pitch_kp[0][1] = 5; //If no kp use 5
+	} else { k.pitch_kp[0][1] = config->kp0; }
 
 	return k;
 }
