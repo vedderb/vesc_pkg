7e9d01663df4e8606ff49239d07c752acff21aa7
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-16 16:24:30 -0700

    Update high_current.h

--- tnt/tnt/high_current.h
+++ tnt/tnt/high_current.h
@@ -17,14 +17,19 @@
 
 #pragma once
 
-	struct {
-	bool traction_control,
-	float wheelslip_timeron,
-	float wheelslip_timeroff,
-	float wheelslip_accelstartval,
-	bool wheelslip_highaccelon1,
-	bool wheelslip_highaccelon2,
-	float wheelslip_lasterpm,
-	float wheelslip_erpm,
-	} TractionData;
+typedef struct {
+	float timer;				//Timer to monitor surge cycle and period
+	bool state;				//Identifies surge state which drives duty to max
+	float new_duty_cycle;			//Used to ramp duty cycle
+	bool state_off;				//Used to identify when setpoint should return to nowmal
+	float tiltback_step_size;		//Speed that the board returns to setpoint
+	float setpoint;				//Setpoint allowed by surge
+	float start_current;			//Current that starts surge
+	float ramp_rate				//Duty cycle ramp rate
+	bool high_current			//A state below surge current by amount, overcurrent margin
+	float high_current_timer		//Limits the duration of haptic buzz
+	bool high_current_buzz			//A state that allows haptic buzz during high current
+} SurgeData;
 
+void check_current(data *d);
+void check_surge(data *d);
