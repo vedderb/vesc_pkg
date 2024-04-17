470d68aa0ebe6735b2b6eacb6f66b57580dc8691
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-17 13:18:58 -0700

    Update tnt.c

--- tnt/tnt/tnt.c
+++ tnt/tnt/tnt.c
@@ -897,7 +897,7 @@ static void tnt_thd(void *arg) {
 			// this keeps the start smooth and predictable
 			if (d->start_counter_clicks == 0) {
 				
-                // Select and Apply Rate P
+                		// Select and Apply Rate P
 				float rate_prop = -d->gyro[1];
 				float kp_rate = brake_curve ? d->brake_kp.kp_rate : d->accel_kp.kp_rate;		
 				float rate_stabl = 1+d->stabl*d->tnt_conf.stabl_rate_max_scale/100; 			
