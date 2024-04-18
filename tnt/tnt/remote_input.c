6811ad3c6238999c9edf7ad5ccf2db13de421af0
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-17 17:58:24 -0700

    Update remote_input.c

--- tnt/tnt/remote_input.c
+++ tnt/tnt/remote_input.c
@@ -125,11 +125,11 @@ void update_remote(tnt_config *config, RemoteData *r) {
 	r->throttle_val = servo_val;
 }
 
-void configure_remote_features(tnt_config *config, RemoteData r, StickyTiltData *s) {
+void configure_remote_features(tnt_config *config, RemoteData *r, StickyTiltData *s) {
 	r->smoothing_factor = r->smoothing_factor;
 	r->step_size = config->inputtilt_speed / config->hertz;
 	r->ramped_step_size = 0;
 	s->low_value = config->stickytiltval1; // Value that defines where tilt will stick for both nose up and down. Can be made UI input later.
 	s->high_value = config->stickytiltval2; // Value of 0 or above max disables. Max value <=  r->angle_limit. 
-	s->hold_current = config->holdcurrent;
+	s->hold_current = config->stickytilt_holdcurrent;
 }
