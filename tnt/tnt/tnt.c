078b23299be12705adebf6c4d572d21139b6dd23
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-17 21:41:27 -0700

    Update tnt.c

--- tnt/tnt/tnt.c
+++ tnt/tnt/tnt.c
@@ -3,14 +3,14 @@
 // Copyright 2023 Michael Silberstein
 // Copyright 2024 Lukas Hrazky
 //
-// This file is part of the Refloat VESC package.
+// This file is part of the Trick and Trail VESC package.
 //
-// Refloat VESC package is free software: you can redistribute it and/or modify
+// This VESC package is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
 //
-// Refloat VESC package is distributed in the hope that it will be useful, but
+// This VESC package is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 // more details.
