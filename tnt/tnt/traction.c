b94e7ad0eb93c80594c5227b41e112aede7fd4fe
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-04-21 19:44:45 -0700

    Update traction.c

--- tnt/tnt/traction.c
+++ tnt/tnt/traction.c
@@ -105,37 +105,37 @@ void check_traction(MotorData *m, TractionData *traction, State *state, RuntimeD
 	}
 }
 
-void check_drop(DropData *drop, RuntimeData *rt){
-	float accel_z_reduction = cosf(deg2rad(rt->roll_angle)) * cosf(deg2rad(rt->pitch_angle));		// Accel z is naturally reduced by the pitch and roll angles, so use geometry to compensate
+void check_drop(DropData *drop, RuntimeData *rt, DropDebug *drop_dbg){
+	float accel_z_reduction = cosf(deg2rad(rt->roll_angle)) * cosf(deg2rad(rt->pitch_angle));			// Accel z is naturally reduced by the pitch and roll angles, so use geometry to compensate
 	if (drop->applied_accel_z_reduction > accel_z_reduction) {							// Accel z acts slower than pitch and roll so we need to delay accel z reduction as necessary
-		drop->applied_accel_z_reduction = accel_z_reduction ;						// Roll or pitch are increasing. Do not delay
+		drop->applied_accel_z_reduction = accel_z_reduction ;							// Roll or pitch are increasing. Do not delay
 	} else {
-		drop->applied_accel_z_reduction = drop->applied_accel_z_reduction * .999 + accel_z_reduction * .001;	// Roll or pitch decreasing. Delay to allow accel z to keep pace	
+		drop->applied_accel_z_reduction = drop->applied_accel_z_reduction * .999 + accel_z_reduction * .001;	// Roll or pitch decreasing. Delay to allow accelerometer to keep pace	
 	}
-	if ((rt->accel[2] < drop->z_limit * drop->applied_accel_z_reduction) && 		// Compare accel z to drop limit with reduction for pitch and roll.
+	
+	if ((rt->accel[2] < drop->z_limit * drop->applied_accel_z_reduction) && 	// Compare accel z to drop limit with reduction for pitch and roll.
 	    (rt->last_accel_z >= rt->accel[2]) &&  					// check that we are still dropping
 	    (state->sat != SAT_CENTERING) && 						//Not during startup
 	    (rt->current_time - drop->timeroff > 0.02)) {				// Don't re-enter drop state for duration 	
 		drop->count += 1;
-		if (drop->count > 5) {							// Counter used to reduce nuisance trips
-			if (!drop->active) { 			// Set the on timer only if it is well past what is normal so it only sets once
+		if (drop->count > drop->count_limit) {					// Counter used to reduce nuisance trips
+			if (!drop->active) { 						// Set the on timer only once per drop
 				drop->timeron = rt->current_time;
 			}
 			drop->active = true;
-			drop_dbg->debug4 = min(drop_dgb->debug4, rt->accel[2]);
+			drop_dbg->debug4 = min(drop_dgb->debug4, rt->accel[2]); /	/record the lowest accel
 		}
-	} else { drop->count = 0; }			// reset
+	} else { drop->count = 0; }							// reset
 	
-	if (drop->active == true) {		// If any conditions are not met while in drop state, exit drop state
-		if (fabsf(m->acceleration) > drop->motor_limit) { //Fastest reaction is hall sensor
+	if (drop->active == true) {					
+		if (fabsf(m->acceleration) > drop->motor_limit) { 	//Fastest reaction is hall sensor
 			drop_deactive(&drop);
 			drop_dbg->debug1 = m->acceleration;
-		} else if (rt->last_accel_z <= rt->accel[2]) {	// for fastest landing reaction check that we are still dropping
+		} else if (rt->last_accel_z <= rt->accel[2]) {		// for fastest landing reaction with accelerometer check that we are still dropping
 			drop_deactive(&drop);
 			drop_dbg->debug1 = rt->accel[2];
 		}
-		
-	} 
+	}
 
 }
 
@@ -143,6 +143,7 @@ void configure_traction(DropData *drop, tnt_config *config){
 	drop->tiltback_step_size = config->tiltback_drop_speed / config->hertz;
 	drop->z_limit = config->drop_z_accel;	// Value of accel z to initiate drop. A drop of about 6" / .1s produces about 0.9 accel y (normally 1)
 	drop->motor_limit = config->drop_motor_accel; //ends drop via motor acceleration
+	drop->count_limit = config->drop_count_limit;
 }
 
 void reset_traction(DropData *drop){
