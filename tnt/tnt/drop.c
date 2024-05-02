8767e9a6fd8a510ed4a534795281d455fa69cc79
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-05-01 18:17:42 -0700

    Update drop.c

--- tnt/tnt/drop.c
+++ tnt/tnt/drop.c
@@ -44,7 +44,10 @@ void check_drop(DropData *drop, MotorData *m, RuntimeData *rt, State *state, Dro
 				}
 				drop->active = true;
 				drop_dbg->debug4 = min(drop_dbg->debug4, drop->accel_z); 	//record the lowest accel
-			} else { drop_dbg->debug1 = rt->current_time; }				//Update to indicate recent drop prevention
+			} else { 
+				drop_dbg->debug1 = rt->current_time; 				//Update to indicate recent drop prevention
+				drop->count = 0
+			}				
 		}
 	} else { drop->count = 0; }							// reset
 	
