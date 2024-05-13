da8e32e7b889c3f95c2b8099c5282a3de323bd1e
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-05-13 11:44:48 -0700

    Update traction.c

--- tnt/tnt/traction.c
+++ tnt/tnt/traction.c
@@ -20,7 +20,7 @@
 #include "utils_tnt.h"
 
 void check_traction(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, TractionDebug *traction_dbg){
-	float erpmfactor = config->wheelslip_scaleaccel - min(config->wheelslip_scaleaccel - 1, (config->wheelslip_scaleaccel -1) * ( m->abs_erpm / config->wheelslip_scaleerpm));
+	float erpmfactor = lerp(0, config->wheelslip_scaleerpm, config->wheelslip_scaleaccel, 1, m->abs_erpm);
 	bool erpm_check;
 	bool start_condition = false;
 
