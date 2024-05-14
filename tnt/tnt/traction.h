12fafdf9ce9ba46535eb790c1b1217767a618136
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-05-13 21:07:22 -0700

    Update traction.h

--- tnt/tnt/traction.h
+++ tnt/tnt/traction.h
@@ -49,5 +49,6 @@ typedef struct {
 } TractionDebug;
 
 void check_traction(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, TractionDebug *traction_dbg);
+void reset_traction(TractionData *traction, State *state);
 void deactivate_traction(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, TractionDebug *traction_dbg);
 void configure_traction(TractionData *traction, tnt_config *config);
