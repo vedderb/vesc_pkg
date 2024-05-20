8fb5cdb76492b5b176f1f6f5d625173fb342e290
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-05-19 19:43:06 -0700

    Update traction.c

--- tnt/tnt/traction.c
+++ tnt/tnt/traction.c
@@ -130,6 +130,6 @@ void configure_traction(TractionData *traction, tnt_config *config){
 	float hertz = config->hertz;
 	float wheelslip_margin = config->wheelslip_margin;
 	traction->start_accel = 1.0 * config->wheelslip_accelstart / hertz * 1000.0;
-	traction->slowed_accel = 1.0 * config->wheeslip_accelend / hertz * 1000.0;
+	traction->slowed_accel = 1.0 * config->wheelslip_accelend / hertz * 1000.0;
 	traction->end_accel_rate = wheelslip_margin / (hertz * hertz) * 1000000.0;
 }
