cea46be2616fc55b0bdaef3f2276be1e9a992c0b
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-05-24 20:25:13 -0700

    Update roll.c

--- tnt/tnt/roll.c
+++ tnt/tnt/roll.c
@@ -17,34 +17,13 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "yaw.h"
 #include "utils_tnt.h"
 #include <math.h>
 
-void calc_yaw_change(YawData *yaw, float yaw_angle, YawDebugData *yaw_dbg){ 
-	float new_change = yaw_angle - yaw->last_angle;
-	if ((new_change == 0) || // Exact 0's only happen when the IMU is not updating between loops
-	    (fabsf(new_change) > 100)) { // yaw flips signs at 180, ignore those changes
-		new_change = yaw->last_change;
-	}
-	yaw->last_change = new_change;
-	yaw->last_angle = yaw_angle;
-	yaw->change = yaw->change * 0.8 + 0.2 * (new_change);
-	yaw->abs_change = fabsf(yaw->change);
-	yaw_dbg->debug1 = yaw->change;
-}
-
-void yaw_reset(YawData *yaw, YawDebugData *yaw_dbg){ 
-	yaw->last_angle = 0;
-	yaw->last_change = 0;
-	yaw->abs_change = 0;
-	yaw_dbg->debug2 = 0;
-}
-/*
-float yaw_erpm_scale(const tnt_config *config, float abs_erpm){ 
-	float erpmscale = lerp(config->yaw_lowerpm, config->yaw_higherpm, 0, config->yaw_maxscale/100, abs_erpm);
+float roll_erpm_scale(const tnt_config *config, float abs_erpm){ 
+	float erpmscale = lerp(config->roll_lowerpm, config->roll_higherpm, 0, config->roll_maxscale/100, abs_erpm);
 	float sign_erpmscale = sign(erpmscale);
 	erpmscale = min(max(fabsf(erpmscale), 0), config->yaw_maxscale/100) * sign_erpmscale;
 	return erpmscale;
 }
-*/
+
