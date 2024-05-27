6afe94d39eb80d519ad07d04504ff540edd6f704
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-05-26 18:41:40 -0700

    Update traction.c

--- tnt/tnt/traction.c
+++ tnt/tnt/traction.c
@@ -133,7 +133,7 @@ void configure_traction(TractionData *traction, tnt_config *config, TractionDebu
 }
 
 void check_traction_braking(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, float inputtilt_interpolated, TractionDebug *traction_dbg){
-	if (-inputtilt_interpolated * m->erpm_sign > config->traction_braking_angle &&
+	if (-inputtilt_interpolated * m->erpm_sign >= config->traction_braking_angle &&
 	    state->braking_pos) {
 		traction->traction_braking = true;
 		
