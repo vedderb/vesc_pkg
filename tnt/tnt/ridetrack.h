9d1871eed59c2a6c556857d5a4666097bb3a3641
Izzygit <126716104+Izzygit@users.noreply.github.com>
2025-05-16 18:49:57 -0700

    Update ridetrack.h

--- tnt/tnt/ridetrack.h
+++ tnt/tnt/ridetrack.h
@@ -57,6 +57,6 @@ void configure_ride_tracking(RideTrackData *ridetrack, tnt_config *config);
 void reset_ride_tracking(RideTrackData *ridetrack);
 void rest_timer(RideTrackData *ridetrack, RuntimeData *rt);
 void ride_timer(RideTrackData *ridetrack, RuntimeData *rt);
-void reset_ride_tracking_on_configure(RideTrackData *ridetrack, tnt_config *config, TractionDebugData *traction_dbg);
+void reset_ride_tracking_on_configure(RideTrackData *ridetrack, tnt_config *config, TractionDebug *traction_dbg);
 void ride_tracking_update(RideTrackData *ridetrack, RuntimeData *rt, YawData *yaw);
 void carve_tracking(RuntimeData *rt, YawData *yaw, RideTrackData *ridetrack);
