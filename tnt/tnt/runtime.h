7edae1727a13725bbe6aac6f8900025a9ff13e24
Izzygit <126716104+Izzygit@users.noreply.github.com>
2025-05-16 18:34:34 -0700

    Update runtime.h

--- tnt/tnt/runtime.h
+++ tnt/tnt/runtime.h
@@ -67,48 +67,9 @@ typedef struct {
 	float debug5; //erpm scaler
 } YawDebugData;
 
-typedef struct {
-	float rest_time;
-	float last_rest_time;
-	float ride_time;
-	float last_ride_time;
-	bool run_flag;
-	float min_yaw_change;
-	float yaw_timer;
-	int8_t last_yaw_sign;
-	int8_t yaw_sign;
-	float carve_timer;
-	uint32_t carve_chain;
-	uint32_t carves_total;
-	uint32_t max_carve_chain;
-	float carves_mile;
-	float distance;
-	float efficiency;
-	float speed_avg;
-	float current_avg;
-	float power_avg;
-	float reset_mileage;
-	float max_roll_temp;
-	float max_roll;
-	float max_roll_avg;
-	float max_yaw_temp;
-	float max_yaw;
-	float max_yaw_avg;
-	float max_time;
-	uint32_t bonks_total;
-} RideTrackData;
-
 void runtime_data_update(RuntimeData *rt);
 void apply_pitch_filters(RuntimeData *rt, tnt_config *config);
 void calc_yaw_change(YawData *yaw, RuntimeData *rt, YawDebugData *yaw_dbg);
 void reset_runtime(RuntimeData *rt, YawData *yaw, YawDebugData *yaw_dbg);
 void configure_runtime(RuntimeData *rt, tnt_config *config);
-void rest_timer(RideTrackData *ridetrack, RuntimeData *rt);
-void ride_timer(RideTrackData *ridetrack, RuntimeData *rt);
 void check_odometer(RuntimeData *rt);
-void configure_ride_tracking(RideTrackData *ridetrack, tnt_config *config);
-void reset_ride_tracking(RideTrackData *ridetrack);
-void reset_ride_tracking_on_configure(RideTrackData *ridetrack, tnt_config *config);
-void ride_tracking_update(RideTrackData *ridetrack, RuntimeData *rt, YawData *yaw);
-void carve_tracking(RuntimeData *rt, YawData *yaw, RideTrackData *ridetrack);
-void ridetrack_traction(RideTrackData *ridetrack, float exit, float time, float abs_erpm);
