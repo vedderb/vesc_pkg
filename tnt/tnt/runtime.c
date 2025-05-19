6f40a07ac40a554cf35606cbe87f7f89d34e63c6
Izzygit <126716104+Izzygit@users.noreply.github.com>
2025-05-19 13:21:43 -0700

    Update runtime.c

--- tnt/tnt/runtime.c
+++ tnt/tnt/runtime.c
@@ -32,13 +32,11 @@ void runtime_data_update(RuntimeData *rt) {
 	rt->last_time = rt->current_time;
 	
 	// Get the IMU Values
-	float roll_rad = VESC_IF->imu_get_roll();
-	rt->roll_angle = rad2deg(roll_rad);
+	rt->roll_angle = rad2deg(VESC_IF->imu_get_roll());
 	rt->abs_roll_angle = fabsf(rt->roll_angle);
 	rt->true_pitch_angle = rad2deg(VESC_IF->ahrs_get_pitch(&rt->m_att_ref)); // True pitch is derived from the secondary IMU filter running with kp=0.2
 	rt->pitch_angle = rad2deg(VESC_IF->imu_get_pitch());
 	VESC_IF->imu_get_gyro(rt->gyro);
-    	rt->gyro_y = cosf(roll_rad) * cosf(roll_rad) * gyro[1] + cosf(roll_rad) * sinf(roll_rad) * gyro[2]; //rt->gyro_y = cosf(roll_rad) * gyro[1] + sinf(roll_rad) * gyro[2];
 	VESC_IF->imu_get_accel(rt->accel); //Used for drop detection
 	rt->yaw_angle = rad2deg(VESC_IF->ahrs_get_yaw(&rt->m_att_ref));
 }
