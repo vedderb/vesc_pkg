7529deb95824867eb50ec563275cb8dfcd92cfa0
Izzygit <126716104+Izzygit@users.noreply.github.com>
2025-06-29 20:12:29 -0700

    Update runtime.c

--- tnt/tnt/runtime.c
+++ tnt/tnt/runtime.c
@@ -136,7 +136,7 @@ void configure_runtime(RuntimeData *rt, tnt_config *config) {
 	
 	// EMA Filter Factor
 	float imu_sample_rate = VESC_IF->get_cfg_float(CFG_PARAM_IMU_sample_rate);
-	rt->ema_factor = 100.0 * imu_sample_rate; // config->hertz;
+	rt->ema_factor = 100.0 * 1 / imu_sample_rate; // config->hertz;
 }
 
 void check_odometer(RuntimeData *rt) { 
